e44819f75f8024544790e1171e0a18a57c8d8509
TzKal-Felipe <119777652+TzKal-Felipe@users.noreply.github.com>
2024-12-06 16:53:24 -0700

    Update game.js

--- res/js/game.js
+++ res/js/game.js
@@ -314,7 +314,7 @@ function startGame() {
     fireX.onChange(() => {
         console.log("X is changing");
         console.log(currentLevel === 1);
-        console.log(nearLocation(allPlayers[0].position, coords.level1.start);
+        console.log(nearLocation(allPlayers[0].position, coords.level1.start));
         if (currentLevel === 1){
             if (nearLocation(allPlayers[0].position, coords.level1.start)){
                 console.log("near start");
