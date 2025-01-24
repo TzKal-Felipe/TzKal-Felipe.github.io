a3ec49a409f4e621799d81c71204cd28aef91742
TzKal-Felipe <119777652+TzKal-Felipe@users.noreply.github.com>
2025-01-24 00:03:24 -0700

    Update audio.js

--- res/js/audio.js
+++ res/js/audio.js
@@ -3,9 +3,14 @@ export class AudioManager {
         this.audio = new Audio();
         this.isPlaying = false;
         this.queue = null;
+        this.currentSrc = null;
     }
 
     playAudio(audioSrc){
+        if (audioSrc === this.currentSrc || audioSrc === this.queue){
+            return;
+        }
+        
         if (this.isPlaying){
             this.queue = audioSrc;
             console.log("audio queued");
@@ -14,9 +19,11 @@ export class AudioManager {
             this.audio.load();
             this.audio.play();
             this.isPlaying = true;
+            this.currentSrc = audioSrc;
 
             this.audio.onended = () => {
                 this.isPlaying = false;
+                this.currentSrc = null;
 
                 if (this.queue){
                     const nextAudio = this.queue;
