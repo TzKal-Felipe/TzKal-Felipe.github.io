2149604d0064ef63e0f3c639bfe41c98ec2a98a9
TzKal-Felipe <119777652+TzKal-Felipe@users.noreply.github.com>
2025-01-24 15:00:35 -0700

    Create helperFunctions.js

---
+++ res/js/helperFunctions.js
@@ -0,0 +1,42 @@
+function nearLocation(currentPos, targetPos, thresholdX = 20, thresholdY = 70){
+    return (
+        Math.abs(currentPos.x - targetPos.x) <= thresholdX &&
+        Math.abs(currentPos.y - targetPos.y) <= thresholdY
+    );
+}
+
+function restartTimer(){
+    timer = 0;
+}
+
+function moveRight(player){
+    player.keys.pressed.right = true;
+}
+
+function moveLeft(player){
+    player.keys.pressed.left = true;
+}
+
+function stopMoving(player){
+    player.keys.pressed.left = false;
+    player.keys.pressed.right = false;
+}
+
+function makeJump(player){
+    player.velocity.y = -4.35;
+    player.keys.pressed.up = true;
+                    
+    setTimeout(() => {
+        player.keys.pressed.up = false;
+    }, 300);
+}
+
+function playAudio(audioSrc){
+    nextAudio = audioSrc;
+    audioManager.playAudio(nextAudio);
+    restartTimer();
+}
+
+function queueAudioForTimer(audioSrc){
+    nextAudio = audioSrc;
+}
