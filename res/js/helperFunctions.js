7e36a5e3b574db746aa7f69d92c703cf9a882ba3
TzKal-Felipe <119777652+TzKal-Felipe@users.noreply.github.com>
2025-02-06 11:00:33 -0700

    Update helperFunctions.js

--- res/js/helperFunctions.js
+++ res/js/helperFunctions.js
@@ -1,24 +1,24 @@
-export function nearLocation(currentPos, targetPos, thresholdX = 20, thresholdY = 70){
+function nearLocation(currentPos, targetPos, thresholdX = 20, thresholdY = 70){
     return (
         Math.abs(currentPos.x - targetPos.x) <= thresholdX &&
         Math.abs(currentPos.y - targetPos.y) <= thresholdY
     );
 }
 
-export function moveRight(player){
+function moveRight(player){
     player.keys.pressed.right = true;
 }
 
-export function moveLeft(player){
+function moveLeft(player){
     player.keys.pressed.left = true;
 }
 
-export function stopMoving(player){
+function stopMoving(player){
     player.keys.pressed.left = false;
     player.keys.pressed.right = false;
 }
 
-export function makeJump(player){
+function makeJump(player){
     player.velocity.y = -4.35;
     player.keys.pressed.up = true;
                     
@@ -26,3 +26,5 @@ export function makeJump(player){
         player.keys.pressed.up = false;
     }, 300);
 }
+
+export { nearLocation, moveRight, moveLeft, stopMoving, makeJump };
