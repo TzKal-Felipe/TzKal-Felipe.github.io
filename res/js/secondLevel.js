14e0e93eea593d09e083b323a649bcc4390a81e5
TzKal-Felipe <119777652+TzKal-Felipe@users.noreply.github.com>
2025-02-06 11:07:22 -0700

    Update secondLevel.js

--- res/js/secondLevel.js
+++ res/js/secondLevel.js
@@ -1,3 +1,47 @@
+import { 
+    nearLocation,
+    moveRight,
+    moveLeft,
+    stopMoving,
+    makeJump,
+} from "./helperFunctions.js";
+
+let audioFiles = {
+    
+};
+
 export class SecondLevelManager {
-  
+    constructor(fireboy, watergirl, audioManager){
+        this.fireboy = fireboy;
+        this.watergirl = watergirl;
+        this.audioManager = audioManager;
+        
+        this.fireCheckpoints = {
+        
+        };
+        
+        this.waterCheckpoints = {
+        
+        };
+        
+        this.coordinates = {
+        
+        };
+    }
+
+    markFireboyCheckpoints(){
+        
+    }
+
+    markWatergirlStopsAndCheckpoints(){
+        
+    }
+
+    checkForWatergirlMovement(){
+        
+    }
+
+    checkForLevelStateActions(){
+        
+    }
 }
