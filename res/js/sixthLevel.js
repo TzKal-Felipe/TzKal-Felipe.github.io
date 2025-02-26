ed84e5a9c3b21c822a0772dcd82811da3b7c9440
TzKal-Felipe <119777652+TzKal-Felipe@users.noreply.github.com>
2025-02-26 14:06:10 -0700

    Update sixthLevel.js

--- res/js/sixthLevel.js
+++ res/js/sixthLevel.js
@@ -11,10 +11,18 @@ let audioFiles = {
 };
 
 export class SixthLevelManager {
-    constructor(fireboy, watergirl, audioManager){
+    constructor(fireboy, watergirl, audioManager, allButtons, allLevers, allBalls){
         this.fireboy = fireboy;
         this.watergirl = watergirl;
         this.audioManager = audioManager;
+        this.blueButton = allButtons[0][0];
+        this.redButton = allButtons[1][0];
+        this.greenButton = allButtons[2][0];
+        this.purpleButton = allButtons[3][0];
+        this.greyLever = allLevers[0];
+        this.leftBall = allBalls[0];
+        this.middleBall = allBalls[1];
+        this.rightBall = allBalls[2];
         
         this.fireCheckpoints = {
         
