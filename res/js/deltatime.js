c01ab43ed35bc6d5eed4e0d12b64e0be04ec8104
TzKal-Felipe <119777652+TzKal-Felipe@users.noreply.github.com>
2025-03-03 03:43:44 -0700

    Create deltatime.js

---
+++ res/js/deltatime.js
@@ -0,0 +1,34 @@
+export class Deltatime {
+    constructor(){
+        this._value = 0;
+        this.eventTarget = new EventTarget();
+        this.update = null;
+        this.intervalRate = 50;
+    }
+
+    get value(){
+        return this._value;
+    }
+
+    set value(newValue){
+        if (this._value !== newValue){
+            this._value = newValue;
+            this.eventTarget.dispatchEvent(new Event("valueChanged"));
+        }
+    }
+
+    onChange(callback){
+        this.eventTarget.addEventListener("valueChanged", callback);
+    }
+
+    start(){
+        this.update = setInterval(() => {
+            this.value = this.value + 1;
+        }, this.intervalRate);
+    }
+
+    stop(){
+        clearInterval(this.update);
+        this.value = 0;
+    }
+}
