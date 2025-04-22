1b7cae7a6d3ef4330a0815a869ddd94d2d7f8a48
TzKal-Felipe <119777652+TzKal-Felipe@users.noreply.github.com>
2025-04-22 11:54:25 -0600

    Update fourthLevel.js

--- res/js/fourthLevel.js
+++ res/js/fourthLevel.js
@@ -25,7 +25,20 @@ export class FourthLevelManager {
         };
         
         this.coordinates = {
-        
+            waterStart: {x: 1100, y: 47},
+            firstWaterDrop: {x: 1280, y: 96},
+            afterFirstWaterDrop: {x: 1280, y: 227},
+            secondWaterDrop: {x: 1192, 243},
+            afterSecondWaterDrop: {x: 1192, y: 352},
+            thirdWaterDrop: {x: 832, y: 485},
+            afterThirdWaterDrop: {x: 833, y: 622},
+            beforeFirstLavaPool: {x: 883, y: 622},
+            fourthWaterDrop: {x: 1160, y: 563},
+            afterFourthWaterDrop: {x: 1160, y: 658},
+            fifthWaterDrop: {x: 1251, y: 692},
+            afterFifthWaterDrop: {x: 1265, y: 766},
+            atRedLever: {x: 1095, y: 766},
+            gapRightOfBluePlatform: {x: 797, y: 527},
         };
     }
 
