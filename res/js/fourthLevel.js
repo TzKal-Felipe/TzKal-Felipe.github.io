f991716a21575f46915fd18ad376c19e066e9f1d
TzKal-Felipe <119777652+TzKal-Felipe@users.noreply.github.com>
2025-05-03 11:51:58 -0600

    Update fourthLevel.js

--- res/js/fourthLevel.js
+++ res/js/fourthLevel.js
@@ -26,9 +26,7 @@ export class FourthLevelManager {
         
         this.waterCheckpoints = {
             firstWaterDrop: false,
-            secondWaterDrop: false,
             thirdWaterDrop: false,
-            fourthWaterDrop: false,
             fifthWaterDrop: false,
             redLever: false,
             platformAfterLever: false,
@@ -82,17 +80,9 @@ export class FourthLevelManager {
             stopMoving(this.watergirl);
             this.waterCheckpoints.firstWaterDrop = true;
         }
-        if (nearLocation(this.watergirl, this.coordinates.secondWaterDrop) && !this.waterCheckpoints.secondWaterDrop){
-            stopMoving(this.watergirl);
-            this.waterCheckpoints.secondWaterDrop = true;
-        }
         if (nearLocation(this.watergirl, this.coordinates.thirdWaterDrop) && !this.waterCheckpoints.thirdWaterDrop){
             stopMoving(this.watergirl);
             this.waterCheckpoints.thirdWaterDrop = true;
-        }
-        if (nearLocation(this.watergirl, this.coordinates.fourthWaterDrop) && !this.waterCheckpoints.fourthWaterDrop){
-            stopMoving(this.watergirl);
-            this.waterCheckpoints.fourthWaterDrop = true;
         }
         if (nearLocation(this.watergirl, this.coordinates.fifthWaterDrop) && !this.waterCheckpoints.fifthWaterDrop){
             stopMoving(this.watergirl);
@@ -139,8 +129,93 @@ export class FourthLevelManager {
         }
     }
 
+    this.coordinates = {
+            waterStart: {x: 1100, y: 47},
+            firstWaterDrop: {x: 1280, y: 96},
+            afterFirstWaterDrop: {x: 1280, y: 227},
+            secondWaterDrop: {x: 1192, y: 243},
+            afterSecondWaterDrop: {x: 1192, y: 352},
+            thirdWaterDrop: {x: 832, y: 485},
+            afterThirdWaterDrop: {x: 833, y: 622},
+            beforeFirstLavaPool: {x: 883, y: 622},
+            fourthWaterDrop: {x: 1160, y: 563},
+            afterFourthWaterDrop: {x: 1160, y: 658},
+            fifthWaterDrop: {x: 1251, y: 692},
+            afterFifthWaterDrop: {x: 1265, y: 766},
+            atRedLever: {x: 1095, y: 766},
+            gapRightOfBluePlatform: {x: 797, y: 527},
+            beforeHugeDropJump: {x: 941, y: 227},
+            beforeBluePlatformDrop: {x: 652, y: 520},
+            afterBluePlatformDrop: {x: 652, y: 622},
+            beforeRedPlatformDrop: {x: 725, y: 665},
+            afterRedPlatformDrop: {x: 725, y: 802},
+            betweenFireAndWaterPools: {x: 374, y: 730},
+            beforeFinalDrop: {x: 51, y: 745},
+            afterFinalDrop: {x: 51, y: 896},
+            beforeFirstAcidPool: {x: 281, y: 874},
+            beforeSecondAcidPool: {x: 461, y: 874},
+            beforeWhiteBarrier: {x: 873, y: 910},
+            secondWhiteButton: {x: 1273, y: 910},
+            fireDoor: {x: 1043, y: 910},
+            waterDoor: {x: 1143, y: 910},
+        };
     controlWatergirlMovement(){
-        
+        if (nearLocation(this.watergirl, this.coordinates.waterStart) && this.blueLever.pressed){
+            moveRight(this.watergirl);
+        }
+        if (nearLocation(this.watergirl, this.coordinates.afterFirstWaterDrop)){
+            moveLeft(this.watergirl);
+        }
+        if (nearLocation(this.watergirl, this.coordinates.afterThirdWaterDrop) && !this.redLever.pressed){
+            makeJump(this.watergirl);
+            moveRight(this.watergirl);
+        }
+        if (nearLocation(this.watergirl, this.coordinates.afterFifthWaterDrop)){
+            moveLeft(this.watergirl);
+        }
+        if (nearLocation(this.watergirl, this.coordinates.atRedLever) && this.redLever.pressed){
+            makeJump(this.watergirl);
+            moveRight(this.watergirl);
+        }
+        if (nearLocation(this.watergirl, this.coordinates.afterFourthWaterDrop) && this.redLever.pressed){
+            makeJump(this.watergirl);
+            moveLeft(this.watergirl);
+        }
+        if (nearLocation(this.watergirl, this.coordinates.afterThirdWaterDrop) && this.redLever.pressed){
+            makeJump(this.watergirl);
+        }
+        if (nearLocation(this.watergirl, this.coordinates.gapRightOfBluePlatform) && this.redLever.pressed){
+            makeJump(this.watergirl);
+            moveRight(this.watergirl);
+        }
+        if (nearLocation(this.watergirl, this.coordinates.afterSecondWaterDrop) && this.redLever.pressed){
+            makeJump(this.watergirl);
+            moveLeft(this.watergirl);
+        }
+        if (nearLocation(this.watergirl, this.coordinates.beforeHugeDropJump)){
+            makeJump(this.watergirl);
+        }
+        if (nearLocation(this.watergirl, this.coordinates.afterBluePlatformDrop)){
+            moveRight(this.watergirl);
+        }
+        if (nearLocation(this.watergirl, this.coordinates.afterRedPlatformDrop)){
+            moveLeft(this.watergirl);
+        }
+        if (nearLocation(this.watergirl, this.coordinates.betweenFireAndWaterPools)){
+            makeJump(this.watergirl);
+        }
+        if (nearLocation(this.watergirl, this.coordinates.afterFinalDrop)){
+            moveRight(this.watergirl);
+        }
+        if (nearLocation(this.watergirl, this.coordinates.beforeFirstAcidPool)){
+            makeJump(this.watergirl);
+        }
+        if (nearLocation(this.watergirl, this.coordinates.beforeSecondAcidPool)){
+            makeJump(this.watergirl);
+        }
+        if (nearLocation(this.watergirl, this.coordinates.secondWhiteButton) && this.fireCheckpoints.fireDoor){
+            moveLeft(this.watergirl);
+        }
     }
 
     checkForLevelStateActions(){
