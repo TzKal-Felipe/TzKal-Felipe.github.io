674e61d72e7a6751d4f029a82025ea1cae834428
TzKal-Felipe <119777652+TzKal-Felipe@users.noreply.github.com>
2025-05-03 12:36:05 -0600

    Update fifthLevel.js

--- res/js/fifthLevel.js
+++ res/js/fifthLevel.js
@@ -25,7 +25,32 @@ export class FifthLevelManager {
         };
         
         this.coordinates = {
-        
+            waterStart: {x: 116, y: 118},
+            leftOfFirepool: {x: 506, y: 118},
+            rightOfFirepool: {x: 651, y: 118},
+            rightOfWaterpool: {x: 871, y: 118},
+            upperBall: {x: 1228, y: 118},
+            beforeSpike: {x: 1171, y: 118},
+            lowerBallDrop: {x: 979, y: 10},
+            afterLowerBallDrop: {x: 979, y: 334},
+            lowerBall: {x: 1100, y: 334},
+            underWaterpool: {x: 905, y: 226},
+            dropToDoors: {x: 77, y: 396},
+            afterDropToDoors: {x: 79, y: 514},
+            underFireDoor: {x: 400, y: 550},
+            lowerLevelsDrop: {x: 985, y: 508},
+            afterLowerLevelsDrop: {x: 985, y: 622},
+            avoidLowerLavaJumpToLeft: {x: 859, y: 658},
+            avoidLowerLavaJumpToRight: {x: 718, y: 694},
+            greenLeverDrop: {x: 70, y: 729},
+            afterGreenLeverDrop: {x: 77, y: 910},
+            rightOfGreenLever: {x: 256, y: 910},
+            atGreenLever: {x: 177, y: 910},
+            exitingGreenLeverJump: {x: 5, y: 838},
+            twoStepsJump: {x: 940, y: 658},
+            lowerLevelExitJump: {x: 1084, y: 586},
+            betweenDoors: {x: 568, y: 406},
+            waterDoor: {x: 780, y: 300},
         };
     }
 
